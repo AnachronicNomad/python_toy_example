9d7c833aef2423e55f07571f15f82856599cf549
anachronicnomad <anachronicnomad@gmail.com>
2019-07-04 23:48:55 -0600

    began implementing 'parent' proc simulation code

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 # file: main.py
 from mpi4py import MPI
 from data_structures.sim_structures import *
+from simulation import sim
 
 def main():
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    print(f"""Hello, I'm process {rank} of {size}""")
+    sim(4, 16, 3, debug=True)
 
 
 if __name__ == '__main__':
---
+++ simulation.py
@@ -0,0 +1,17 @@
+# file simulation.py 
+from mpi4py import MPI
+from data_structures.sim_structures import *
+
+def sim(num_planes, num_bins_per_plane, max_particles_per_bin, 
+        r=(0.5, 1.5), z=(0,1),
+        debug=False):
+    """
+        Every process will be running this function. As a result, this func
+        needs to check for its rank and determine what it is actually doing.
+    """
+    comm_world = MPI.COMM_WORLD
+
+    num_procs = comm_world.Get_size()
+    rank = comm_world.Get_rank()
+
+    print(f"""Hello, I'm process {rank} of {num_procs}""")
