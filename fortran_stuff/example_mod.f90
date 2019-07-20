d708dd996672a522f3e246a053e86254e22f5ae4
anachronicnomad <anachronicnomad@gmail.com>
2019-07-19 23:38:25 -0600

    tried to add dynamic window, segfault

--- fortran_stuff/example_mod.f90
+++ fortran_stuff/example_mod.f90
@@ -4,20 +4,49 @@ module example_mod
 
   implicit none
   ! MPI 
-  integer :: procno, numprocs, mpi_err
+  integer :: procno, numprocs, sml_comm, mpi_err
+  integer :: win
+  integer, dimension(:), allocatable :: shared_data
+
+  ! Sim data
+  integer :: loop_cntr
+  integer, dimension(:), allocatable :: local_data
 
 contains
   subroutine simulation_init()
+    !
     call MPI_INIT(mpi_err)
+    call MPI_Comm_dup(MPI_COMM_WORLD, sml_comm, mpi_err)
     call MPI_COMM_RANK(MPI_COMM_WORLD, procno, mpi_err)
     call MPI_COMM_SIZE(MPI_COMM_WORLD, numprocs, mpi_err)
 
-    print *, 'proc',  procno
+    allocate(local_data(1:10))
 
+    call MPI_Win_create_dynamic(MPI_INFO_NULL, sml_comm, win, mpi_err)
+    !
+  end subroutine
+
+  subroutine sim()
+    !
+    do loop_cntr=1,10
+      local_data(loop_cntr) = procno * loop_cntr
+    enddo
+
+    print *, 'proc', procno, 'has', local_data
+
+    call MPI_Win_attach(win, local_data, 10, mpi_err)
+    !
   end subroutine
 
   subroutine teardown()
+    !
+    call MPI_Win_detach(win, local_data)
+    deallocate(local_data)
+
+    call MPI_Win_free(win, mpi_err)
+
     call MPI_FINALIZE(mpi_err)
+    !
   end subroutine
 
 
@@ -30,6 +59,8 @@ program example_prog
    implicit none
 
    call simulation_init()
+
+   call sim()
 
    call teardown()
 
